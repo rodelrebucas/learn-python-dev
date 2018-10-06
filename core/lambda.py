0262035061bf9d6facecff4f31748246257df119
rodel <rodelrebucas@gmail.com>
2018-10-06 20:58:34 +0800

    add lambda samples

---
+++ core/lambda.py
@@ -0,0 +1,16 @@
+'''
+    Lambda or anonymous functions
+        - can have any no. of arguments
+        - only one expression
+        - returns a function object
+'''
+
+# function
+def multiply_to_two(num)
+    return num * 2
+
+print(multiply_to_two(2))
+
+# lambda
+mul = lambda x ; return x * 2
+print(mul(2))
