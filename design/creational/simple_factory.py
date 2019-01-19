b1346753675a7e56400a71be579640f105e8ef66
rodel <rodelrebucas@gmail.com>
2019-01-19 20:57:41 +0800

    update patterns

--- design/creational/simple_factory.py
+++ design/creational/simple_factory.py
@@ -1,82 +1,28 @@
-# - Simple factory pattern is responsible for creating objects *with different types*.
-# - Subclass descdes what kind  of object is instantiated
-# - Creates only one product
-
-### Simple factory pattern
 from abc import ABCMeta, abstractmethod
 
-# Sections, a base class = Product
-class Section(metaclass=ABCMeta):
+
+class Animal(metaclass=ABCMeta):
     @abstractmethod
-    def describe(self):
+    def do_say(self):
         pass
 
 
-# Concrete Product
-class PersonalSection(Section):
-    def describe(self):
-        print("Personal Section")
+class dog(Animal):
+    def do_say(self):
+        print("bark...")
 
 
-class AlbumSection(Section):
-    def describe(self):
-        print("Album Section")
+class cat(Animal):
+    def do_say(self):
+        print("Meow...")
 
 
-class PatentSection(Section):
-    def describe(self):
-        print("Patent Section")
-
-
-class PublicationSection(Section):
-    def describe(self):
-        print("PUblication Section")
-
-
-# Creator, an abstrast class to be implemented by
-# the concrete creators
-class Profile(metaclass=ABCMeta):
-    def __init__(self):
-        self.sections = []
-        self.createProfile()
-
-    # creators' *factory method
-    @abstractmethod
-    def createProfile(self):
-        pass
-
-    def getSections(self):
-        return self.sections
-
-    def addSections(self, section):
-        self.sections.append(section)
-
-
-# Concrete creators
-# Implements Profile to create the object desired
-class linkedin(Profile):
-    # Concrete creators' *factory method
-    def createProfile(self):
-        self.name = "LinkedIn"
-        self.addSections(PersonalSection())
-        self.addSections(PatentSection())
-        self.addSections(PublicationSection())
-
-
-class facebook(Profile):
-    def createProfile(self):
-        self.name = "Facebook"
-        self.addSections(PersonalSection())
-        self.addSections(AlbumSection())
+class ForestFactory(object):
+    def make_sound(self, animal):
+        return eval(animal)().do_say()
 
 
 if __name__ == "__main__":
-    profile_type1 = "facebook"  # input("Name your profile[Facebook or LinkedIn]")
-    profile1 = eval(profile_type1.lower())()  # create the instance
-    print(f"Created instance of profile: {profile1.name}:")
-
-    profile_type2 = "linkedin"
-    profile2 = eval(profile_type2.lower())()
-    print(f"Created instance of profile: {profile2.name}:")
-
-    print(f"Created instances of profiles: {profile1.name, profile2.name}:")
+    forest = ForestFactory()
+    animal_type = input("animal type[dog/cat]? ")
+    forest.make_sound(animal_type)
