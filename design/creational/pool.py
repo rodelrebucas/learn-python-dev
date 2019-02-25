7ffadf44feb4117f247b94be38381d66a7a39729
rodel <rodelrebucas@gmail.com>
2019-02-26 02:03:51 +0800

    add pool to  creational

--- design/creational/pool.py
+++ design/creational/pool.py
@@ -1,54 +1,38 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-*What is this pattern about?
-This pattern is used when creating an object is costly (and they are
-created frequently) but only a few are used at a time. With a Pool we
-can manage those instances we have as of now by caching them. Now it
-is possible to skip the costly creation of an object if one is
-available in the pool.
-A pool allows to 'check out' an inactive object and then to return it.
-If none are available the pool creates one to provide without wait.
-*What does this example do?
-In this example queue.Queue is used to create the pool (wrapped in a
-custom ObjectPool object to use with the with statement), and it is
-populated with strings.
-As we can see, the first string object put in "yam" is USED by the
-with statement. But because it is released back into the pool
-afterwards it is reused by the explicit call to sample_queue.get().
-Same thing happens with "sam", when the ObjectPool created insided the
-function is deleted (by the GC) and the object is returned.
-*Where is the pattern used practically?
-*References:
-http://stackoverflow.com/questions/1514120/python-implementation-of-the-object-pool-design-pattern
-https://sourcemaking.com/design_patterns/object_pool
-*TL;DR80
-Stores a set of initialized objects kept ready to use.
-
-
-
-
-"""
+# Use a resource/object if it is already available in the pool - avoid unnecessary creation
+# Get object/resource from a pool - Queue implemented
+# Return back an object to the pool if it is not used
 
 
 class ObjectPool(object):
     def __init__(self, queue, auto_get=False):
-        self._queue = queue
+        print("__init__")
+
+        self._queue = queue  # Queue
+        # Get/Extract first item (FIFO)
         self.item = self._queue.get() if auto_get else None
 
     def __enter__(self):
+        # processing here
+        print("__enter__")
         if self.item is None:
             self.item = self._queue.get()
         return self.item
 
     def __exit__(self, Type, value, traceback):
+        # Upon exit , put the item back to the queue
+        # __exit__ is called when context manager WIth statement is *done
+        print("__exit__")
         if self.item is not None:
+            print("exiting...")
             self._queue.put(self.item)
             self.item = None
 
+    # Garbage collection, called everytime
     def __del__(self):
+        print("__del__")
         if self.item is not None:
+            print("deleting...")
+            # return back the item to the queue
             self._queue.put(self.item)
             self.item = None
 
@@ -59,26 +43,28 @@ def main():
     except ImportError:  # python 2.x compatibility
         import Queue as queue
 
+    sample_queue = queue.Queue()
+    sample_queue.put("yam")  # push to queue
+
+    # context manager - ObjectPool - use a resource
+    # then relase after exit
+    with ObjectPool(sample_queue) as obj:
+        print("Inside with: {}".format(obj))
+    print("Outside with: {}".format(sample_queue.get()))
+
     def test_object(queue):
         pool = ObjectPool(queue, True)
-        print('Inside func: {}'.format(pool.item))
+        print("Inside func: {}".format(pool.item))
 
-    sample_queue = queue.Queue()
-
-    sample_queue.put('yam')
-    with ObjectPool(sample_queue) as obj:
-        print('Inside with: {}'.format(obj))
-    print('Outside with: {}'.format(sample_queue.get()))
-
-    sample_queue.put('sam')
+    sample_queue.put("sam")
     test_object(sample_queue)
-    print('Outside func: {}'.format(sample_queue.get()))
+    print("Outside func: {}".format(sample_queue.get()))
 
     if not sample_queue.empty():
         print(sample_queue.get())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 ### OUTPUT ###
