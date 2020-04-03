a63b3b8feae4cab8f75213f4160fd10274245f3b
rodel <rodelrebucas@gmail.com>
2020-04-04 05:02:25 +0800

    [wip] bubble sort

---
+++ algorithms/sorting/bubble.py
@@ -0,0 +1,21 @@
+"""
+Bubble sort
+
+    Swap elements until its in the right place
+    Sort ascending: [3, 2, 1]
+    a. 1st iteration
+        [2, 3, 1]
+        [2, 1, 3]
+    b. 2nd iteration: [2, 1, 3]
+        [1, 2, 3]
+"""
+
+
+def bubbleSort():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+
+    def testBubbleSort():
+        pass
