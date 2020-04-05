657ebbea1d2ce00f20f60e573370fbcb3cfb82f4
rodel <rodelrebucas@gmail.com>
2020-04-05 23:49:57 +0800

    bubble sort

--- algorithms/sorting/bubble.py
+++ algorithms/sorting/bubble.py
@@ -10,12 +10,33 @@ Bubble sort
         [1, 2, 3]
 """
 
+import pytest
 
-def bubbleSort():
-    raise NotImplementedError
+xfail = pytest.mark.xfail
 
 
-if __name__ == "__main__":
+def bubbleSort(l):
+    # outer iterations is len(l) - 1
+    # e.g [5,4,3,2,1] = 1, 2, 3, 4 = iterations
+    # this makes sure j + 1 won't exceed with list length
+    for i in range(len(l) - 1, 0, -1):
+        for j in range(i):
+            if l[j] > l[j + 1]:
+                l[j], l[j + 1] = l[j + 1], l[j]
+    return l
 
-    def testBubbleSort():
-        pass
+
+@pytest.fixture(
+    params=[
+        {"input": [3, 2, 1], "expected": [1, 2, 3]},
+        {"input": [1, 2, 3, 4, 5], "expected": [1, 2, 3, 4, 5]},
+        {"input": [3, 2, 1, 1, 2, 2, 2, 4], "expected": [1, 1, 2, 2, 2, 2, 3, 4]},
+    ]
+)
+def testCase(request):
+    return request.param
+
+
+def test_bubblesort(testCase):
+    assert bubbleSort(testCase["input"]) == testCase["expected"]
+
