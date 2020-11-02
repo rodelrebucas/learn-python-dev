06ea89e0765deba90cf97d759389176a02b9ec66
rodel <rodelrebucas@gmail.com>
2020-11-03 01:09:53 +0800

    bst and binary search

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -1,137 +1,26 @@
-"""
-    Binary search.
 
-    Time Complexity: O(logN), balanced tree O(n)
-"""
-
-
-class Node(object):
-    def __init__(self, data):
-        self.data = data
-        self.leftChild = None
-        self.rightChild = None
-
-
-class BinarySearchTree(object):
-    def __init__(self):
-        self.root = None
-
-    def insert(self, data):
-        if not self.root:  # insert first node
-            self.root = Node(data)
-        else:
-            self.insertNode(data, self.root)  # insert succeeding nodes
-
-    def remove(self, data):
-        # make sure your tree contains data
-        if self.root:
-            self.root = self.removeNode(data, self.root)  # return the updated nodes
-
-    def insertNode(self, data, node):
-        if data < node.data:
-            if node.leftChild:  # traverse to the existing left child
-                self.insertNode(data, node.leftChild)
-            else:  # set left child
-                node.leftChild = Node(data)
-        else:
-            if node.rightChild:  # traverse to the existing right child
-                self.insertNode(data, node.rightChild)
-            else:  # set right child
-                node.rightChild = Node(data)
-
-    def removeNode(self, data, node):
-
-        if not node:  # an empty node, not found
-            print("data not found...")
-            return node
-        elif data < node.data:  # recurse left until data == node.data
-            node.leftChild = self.removeNode(data, node.leftChild)
-        elif data > node.data:  # recurse right until data == node.data
-            node.rightChild = self.removeNode(data, node.rightChild)
-        else:  # data == node.data
-            # node is a leaf node
-            if not node.leftChild and not node.rightChild:
-                del node
-                return None
-            if not node.leftChild:  # node has only right child
-                tempNode = node.rightChild
-                del node
-                return tempNode
-            elif not node.rightChild:
-                tempNode = node.leftChild
-                del node
-                return tempNode
-
-            # node has both right and left child
-            # get the predecessor or the highest on left child
-            # OR get successor the node after the target node
-
-            # succesor method
-            tempNode = self.getPredecessor(node.leftChild)
-            # replace the data from the predecessor
-            node.data = tempNode.data
-            # delete the predecessor node, recurse
-            node.leftChild = self.removeNode(tempNode.data, node.leftChild)
-        return node
-
-    def getPredecessor(self, node):
-
-        if node.rightChild:
-            return self.getPredecessor(node.rightChild)
-
-        return node
-
-    def getMinValue(self):
-        if self.root:
-            return self.getMin(self.root)
-
-    def getMin(self, node):
-        if node.leftChild:
-            return self.getMin(node.leftChild)
-
-        return node.data
-
-    def getMaxValue(self):
-        if self.root:
-            return self.getMax(self.root)
-
-    def getMax(self, node):
-        if node.rightChild:
-            return self.getMax(node.rightChild)
-
-        return node.data
-
-    def traverse(self):
-        if self.root:
-            self.traverseInOrder(self.root)
-
-    def traverseInOrder(self, node):
-        if node.leftChild:  # if a left child exist traverse
-            self.traverseInOrder(node.leftChild)
-
-        # print current root node
-        print(node.data)
-
-        if node.rightChild:  # if a right child exist traverse
-            self.traverseInOrder(node.rightChild)
-
-
+# Time complexity - log2N
+# For every length of N of array
+# the number of times to search for the item is log2N
+# E.g length 16 = log2N = log2 (4) + 1 since we need to compare the single item remaining in worst case scenerio
+def binarySearch(arr, searchVal):
+    minv = 0
+    maxv = len(arr) - 1
+    while(maxv >= minv):
+        guess = int((minv + maxv)/ 2)
+        if (arr[guess] == searchVal):
+            print("Found")
+            return
+        # Guess lower
+        if (arr[guess] > searchVal):
+            maxv = guess - 1
+        # Guess higher
+        if (arr[guess] < searchVal):
+            minv = guess + 1
+    print("Not found") 
+    return
+    
 if __name__ == "__main__":
-
-    bst = BinarySearchTree()
-    bst.insert(10)
-    bst.insert(13)
-    bst.insert(5)
-    bst.insert(14)
-
-    print(f"Max value: { bst.getMaxValue()}")
-    print(f"Min value: {bst.getMinValue()}")
-    print(bst.traverse())
-
-    num = 100
-    print(f"Removing {num}")
-    bst.remove(num)
-
-    print(f"Max value: { bst.getMaxValue()}")
-    print(f"Min value: {bst.getMinValue()}")
-    print(bst.traverse())
+    s = 1
+    arr = [1,2,3,4,5,6,7,8,10,12,34,67,11]
+    binarySearch(arr, s)
