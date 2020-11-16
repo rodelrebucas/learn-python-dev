fb34cb9e796bbb0e694aee019dac14b7c5980eb4
rodel <rodelrebucas@gmail.com>
2020-11-16 22:31:59 +0800

    quicksort rough pseudo code

--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -1,3 +1,18 @@
+"""
+    Quicksort - sorting by dividing and conquer.
+    Time complexity O(Nlog2^N)
+
+    Pseudo-code:
+        1. Choose a pivot - a value to be consider as a basis for comparison of lower and higher.
+            pivot = len(array) - 1
+        2. Move values lower than the pivot at the lower part of the array and move values
+           greaterthan the pivot at higher part of the array.
+           ....Details (wip)
+        3. Place the pivot at the correct place when no unsorted values for lower and higher exist.
+           Logically it should be after the lower part and before the higher part,
+           since: lower values <= pivot =< higher values
+"""
+
 # import pytest
 
 # @pytest.fixture(
